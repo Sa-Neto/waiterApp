f0b2b48cf78afc5902fbe1118bac5bdb02d075a7
unknown <Sa-Neto>
2023-10-11 11:58:48 -0300

    initial main

---
+++ app/src/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+export default function Main(){
+  return(
+    <Container>
+      <Text>Main</Text>
+    </Container>
+  );
+}
